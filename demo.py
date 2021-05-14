a9970d770863e712c5cbe772ab7550d6e5998858
Charlie O'Hara <charlie.b.ohara@gmail.com>
2021-05-14 13:49:09 -0700

    update demo with plot

--- demo.py
+++ demo.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.datasets import load_breast_cancer
 
 data = load_breast_cancer()
@@ -17,3 +18,6 @@ print('show summary stats for each column')
 print(df.describe())
 print('check target variable properties')
 print(df['target'].value_counts())
+
+df.plot.scatter(x='mean radius', y='mean area')
+plt.show()
